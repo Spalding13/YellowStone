9267bc7740cd1a2499a14f52f67ba10734e62dc5
Ilian Lalov <ilianlalov@gmail.com>
2019-02-20 20:45:37 +0000

    Add env variable for DB url

--- app.js
+++ app.js
@@ -13,8 +13,14 @@ var express     = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
-    
-mongoose.connect("mongodb+srv://admin:Iguanaman_1@cluster0-0oqbe.mongodb.net/test?retryWrites=true",{ useNewUrlParser: true });
+
+console.log(process.env.DATABASEURL);
+//mongoose.connect("mongodb+srv://admin:Iguanaman_1@cluster0-0oqbe.mongodb.net/test?retryWrites=true",{ useNewUrlParser: true });
+//mongoose.connect("mongodb://localhost/yelp_camp",{ useNewUrlParser: true });
+
+mongoose.connect(process.env.DATABASEURL,{ useNewUrlParser: true });
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
